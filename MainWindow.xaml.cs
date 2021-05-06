948097bb30ae87e9c9418b6ed94c933726fc6faa
FILOBATEIR3152 <83763093+FILOBATEIR3152@users.noreply.github.com>
2021-05-06 21:31:46 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string hash = "f0xle";
+
+        private void encript_Click(object sender, RoutedEventArgs e)
+        {
+            Byte[] data = UTF8Encoding.UTF8.GetBytes(text1.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using(TripleDESCng tripleDES = new TripleDESCng()
+                {
+                    Key =keys , Mode = CipherMode.ECB , Padding = PaddingMode.PKCS7
+                })
+                {
+                    ICryptoTransform transform = tripleDES.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    text2.Text = Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+        }
+
+        private void discript_Click(object sender, RoutedEventArgs e)
+        {
+            Byte[] data = Convert.FromBase64String(text2.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCng tripleDES = new TripleDESCng()
+                {
+                    Key = keys,
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                })
+                {
+                    ICryptoTransform transform = tripleDES.CreateDecryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    text3.Text = UTF8Encoding.UTF8.GetString(results);
+                }
+            }
+        }
+    }
+}
